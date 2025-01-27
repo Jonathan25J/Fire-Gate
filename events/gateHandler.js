fb515349667a4ac4593de5c6ff94f2f020d20b21
Jonathan25J <ik.ben.jona4@gmail.com>
2025-01-27 10:33:35 +0100

    its now possible to send image urls as images

--- events/gateHandler.js
+++ events/gateHandler.js
@@ -25,14 +25,27 @@ module.exports = {
 
         const spirit = await spiritService.getSpiritByName(gateway.spirit_name);
 
+        const messageContent = message.content;
+        const messageHasContent = messageContent.trim().length > 0;
+
         for (const user of gatewayUsers) {
             const receiver = await message.client.users.fetch(user);
 
             const embed = new EmbedBuilder()
                 .setTitle(`${spirit.name}`)
                 .setThumbnail(spirit.avatar)
-                .setDescription(message.content)
                 .setColor(spirit.color);
+
+            if (messageHasContent && isValidURL(messageContent) ) {
+                const isImage = await isImageUrl(messageContent);
+                if (isImage) {
+                    embed.setImage(messageContent);
+                } else {
+                    embed.setDescription(messageContent);
+                }
+            } else if (messageHasContent) {
+                embed.setDescription(messageContent);
+            }
 
             if (message.attachments.size > 0 && message.attachments.first().contentType.startsWith('image')) {
                 embed.setImage(message.attachments.first().url);
@@ -44,3 +57,19 @@ module.exports = {
         }
     },
 };
+
+function isValidURL(string) {
+    const urlPattern = new RegExp('^(https?:\\/\\/)?' +
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$', 'i')
+    return urlPattern.test(string);
+}
+
+async function isImageUrl(url) {
+    const res = await fetch(url);
+    const buff = await res.blob();
+    return buff.type.startsWith('image/')
+}
